705096bff641f9bba8d9802b460273a27400489a
j0n0curry <58121289+j0n0curry@users.noreply.github.com>
2022-10-24 16:40:01 +0100

    Update ePCR_view_v1.py

--- ePCR_view_v1.py
+++ ePCR_view_v1.py
@@ -254,12 +254,12 @@ if len(comp) > 0:
     comp['VIC_RFU'] = comp['VIC_RFU'].astype('float').abs()
     comp['ROX_RFU'] = comp['ROX_RFU'].astype('float').abs()
     #will remove this function - to the bottom - allow files to be processed and user defined and downloaed as whole set and analysed sets. 
-    comp.sort_values('Run_ID', inplace = True)
+    comp.sort_values(['Run_ID', 'Row_ID', 'Col_ID'], inplace = True)
     #process file attributes in to parameters for QC. Essential information. 
     comp['Well'] = comp['Row_ID']+comp['Col_ID']
     comp['norm_RNaseP'] =  comp['VIC_RFU'].abs() / comp['ROX_RFU']
     comp['norm_N_Cov'] =  comp["FAM_RFU"]  / comp['ROX_RFU']
-    comp.index.names=['order']
+    #comp.index.names=['order']
     comp.reset_index(inplace = True)
     comp['date_time'] = pd.to_datetime(comp['date_time'], format='%Y%m%d%H%M%S')
     comp[['date', 'time']] = comp['date_time'].astype(str).str.split(' ', 1, expand=True)
